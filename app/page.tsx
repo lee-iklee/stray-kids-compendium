7ac9d3057b5db90adbd10626cd6146bce7f65549
iklee-lee <ikescd@gmail.com>
2025-03-19 18:40:39 +0100

    test

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React, { useState } from 'react';
+
+function App() {
+	return (
+		<div>
+			<main style={{ flexGrow: 1 }}>{/* Contenu principal */}</main>
+		</div>
+	);
+}
+
+export default App;
